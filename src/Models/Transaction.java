5f7598e81c7a711621490df0fc6fae3d88bf0fa9
sashminda iranga <sashmindaps4@gmail.com>
2024-01-24 22:12:53 +0530

    read method added

--- src/Models/Transaction.java
+++ src/Models/Transaction.java
@@ -1,5 +1,11 @@
 package Models;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transaction {
 
     private int transactionId;
@@ -8,6 +14,18 @@ public class Transaction {
     private TransactionType type;
     private String note;
     private boolean isRecurring;
+
+    public Transaction() {
+    }
+
+    public Transaction(int transactionId, double amount, String category, TransactionType type, String note, boolean isRecurring) {
+        this.transactionId = transactionId;
+        this.amount = amount;
+        this.category = category;
+        this.type = type;
+        this.note = note;
+        this.isRecurring = isRecurring;
+    }
 
     public int getTransactionId() {
         return transactionId;
@@ -56,5 +74,33 @@ public class Transaction {
     public void setRecurring(boolean recurring) {
         isRecurring = recurring;
     }
+
+    public List<Transaction> readTransactionsFromCSV(String filePath) {
+        List<Transaction> transactions = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+
+                int transactionId = Integer.parseInt(parts[0].trim());
+                double amount = Double.parseDouble(parts[1].trim());
+                String category = parts[2].trim();
+                TransactionType type = TransactionType.valueOf(parts[3].trim());
+                String note = parts[4].trim();
+                boolean isRecurring = Boolean.parseBoolean(parts[5].trim());
+
+                Transaction transaction = new Transaction(transactionId, amount, category, type, note, isRecurring);
+                transactions.add(transaction);
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        return transactions;
+    }
+
+
+
 }
 
